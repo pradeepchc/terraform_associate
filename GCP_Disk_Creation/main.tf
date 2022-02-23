5b7bea83b79e18007379472587772160a6faa826
Pradeep C.H. Chittoor Hemantha <pradeep.ch@cloudsufi.com>
2022-02-24 01:05:32 +0530

    create disk using tcloud

--- GCP_Disk_Creation/main.tf
+++ GCP_Disk_Creation/main.tf
@@ -1,3 +1,23 @@
+terraform {
+
+  backend "remote" {
+    organization = "pradeep-terraform-cloud"
+    workspaces {
+      name = "terraform_associate"
+    }
+  }
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.11.0"
+    }
+  }
+}
+
+provider "google" {
+  project = "sct-management-console"
+}
+
 resource "google_compute_disk" "default" {
   name  = "pradeep-disk1"
   type  = "pd-ssd"
