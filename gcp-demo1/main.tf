e0901f110cda1f153f73b9cc9f52c2b34f62f27f
Pradeep C.H. Chittoor Hemantha <pradeep.ch@cloudsufi.com>
2022-03-10 19:38:27 +0530

    newdemo

---
+++ gcp-demo1/main.tf
@@ -0,0 +1,30 @@
+terraform {
+  backend "remote" {
+    organization = "pradeep-terraform-cloud"
+    workspaces {
+      name = "terraform_associate"
+    }
+  }
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.11.0"
+    }
+  }
+}
+
+provider "google" {
+  project = "sct-management-console"
+
+resource "google_compute_network" "this" {
+    auto_create_subnetworks = false
+    name = "demo"
+    routing_mode = "REGIONAL"
+}
+
+resource "google_compute_subnetwork" "this" {
+    name = "demo-subnet"
+    ip_cidr_range = "192.168.24.0/24"
+    region = "europe-west1"
+    network = google_compute_network.this.id
+}
